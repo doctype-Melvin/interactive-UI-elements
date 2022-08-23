0aa18a98febc935f615978849d5ff58e5e42aa17
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-23 18:11:28 +0200

    module creates mobile icons and appends to dedicated html element

--- src/mobile.js
+++ src/mobile.js
@@ -1,15 +1,24 @@
 import sun from './sun-weather-sunny-svgrepo-com.svg'
 import talk from './talk-chat-message-svgrepo-com.svg'
+import waves from './waves-sea-ocean-svgrepo-com.svg'
+import calendar from './week-calendar-holiday-svgrepo-com.svg'
+import whirlpool from './whirlpool-sea-water-svgrepo-com.svg'
+
 const mobileContainer = document.querySelector('.mobile')
+let icons = [sun, talk, waves, calendar, whirlpool]
 
-const sunPic = new Image();
-sunPic.style.width = '50px'
-sunPic.src = sun
+function createIcon(image) {
+    const icon = new Image()
+    icon.style.width = '50px'
+    icon.src = image
+    icon.classList.add('img')
+    icon.addEventListener('mouseover', () => icon.style.cursor = 'pointer')
+    icon.style.marginRight = '10px'
+    return icon
+}
 
-const talkPic = new Image();
-talkPic.style.width = '50px'
-talkPic.src = talk
+function appendIcons(array) {
+    return array.map(item => mobileContainer.append(createIcon(item)))
+}
 
-mobileContainer.append(sunPic, talkPic)
-
-// Write fn that creates images
+appendIcons(icons)
